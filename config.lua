5fb5c2afcdab8a9f167d90ee335ecfc887998aa4
Gustavo Kobielski <97350058+GustavoKobielski@users.noreply.github.com>
2023-11-22 19:57:34 -0300

    Novo config.lua

--- config.lua
+++ config.lua
@@ -3,16 +3,18 @@ cfg = {}
 
 
 
-cfg.useNui = true
-cfg.useBlip = false
+cfg.useNui = false
+cfg.useBlip = true
 
 
 cfg.locWork = { -- Localizações das arenas
     vec3(119.69,-344.55,42.97)
 }
 
+cfg.botaoTrabalho = 47 -- [G]
+
 cfg.drawTxt = function()
-    DrawTxtB("PRESSIONE  ~r~E~w~  PARA INICIAR O TRABALHO DE INVESTIGAÇÃO",4,0.5,0.93,0.50,255,255,255,180)
+    DrawTxtB("PRESSIONE ~b~[G] ~w~PARA ~b~INICIAR ~w~O SERVIÇO DE INVESTIGAÇÃO",4,0.5,0.93,0.50,255,255,255,180)
 end
 
 cfg.drawMarker = function(coords) -- DRAWMARKER DO BLIP 
@@ -32,18 +34,67 @@ cfg.textosAleatorios = {
     -- Adicione mais textos aqui
 }
 
---[[
+cfg.level = {
+    ganhoXp = 2,
+    dinheiroAumenta = 1000,
+    XpNescessarioAumenta = 350,
+}
+
+cfg.Casos = {
+    [1] = {
+        -- ['entrada'] = {-1540.02,420.74,110.02},
+        ['servicos'] = {
+            [1] = {-1509.5,400.76,107.49, 'faca', "amb@world_human_janitor@male@idle_a", "idle_a", "prop_tool_broom", 49, 28422 },
+            [2] = {-1549.88,395.4,108.43, 'pa', "amb@world_human_bum_wash@male@high@base", "base"},
+            [3] = {-1529.53,424.58,109.76, 'arma', "amb@code_human_wander_gardener_leaf_blower@idle_a", "idle_b", "prop_leaf_blower_01", 49, 28422},
+            [4] = {-1541.93,433.4,109.2, 'sangue', "amb@code_human_wander_gardener_leaf_blower@idle_a", "idle_b", "prop_leaf_blower_01", 49, 28422}
+        }
+    },
+    [2] = {
+        -- ['entrada'] = {1060.54,-378.09,68.24},
+        ['servicos'] = {
+            [1] = {1047.95,-372.4,67.85, 'Soldar', "WORLD_HUMAN_WELDING" },
+            [2] = {1044.33,-372.88,67.9, 'Plantar semente', "amb@world_human_gardener_plant@female@base", "base_female"},
+            [3] = {1065.64,-377.23,67.92, 'Soprar folhas', "amb@code_human_wander_gardener_leaf_blower@idle_a", "idle_b", "prop_leaf_blower_01", 49, 28422},
+            [4] = {1049.79,-369.9,68.24, 'Consertar maçaneta', "amb@prop_human_parking_meter@female@idle_a", "idle_a_female" }
+        }
+    },
+
+    [3] = {
+        -- ['entrada'] = {1060.54,-378.09,68.24},
+        ['servicos'] = {
+            [1] = {2040.13,-372.4,67.85, 'Soldar', "WORLD_HUMAN_WELDING" },
+            [2] = {1044.33,-372.88,67.9, 'Plantar semente', "amb@world_human_gardener_plant@female@base", "base_female"},
+            [3] = {1065.64,-377.23,67.92, 'Soprar folhas', "amb@code_human_wander_gardener_leaf_blower@idle_a", "idle_b", "prop_leaf_blower_01", 49, 28422},
+            [4] = {1049.79,-369.9,68.24, 'Consertar maçaneta', "amb@prop_human_parking_meter@female@idle_a", "idle_a_female" }
+        }
+    },
+
+    [4] = {
+        -- ['entrada'] = {1060.54,-378.09,68.24},
+        ['servicos'] = {
+            [1] = {47.95,-372.4,67.85, 'Soldar', "WORLD_HUMAN_WELDING" },
+            [2] = {1044.33,-372.88,67.9, 'Plantar semente', "amb@world_human_gardener_plant@female@base", "base_female"},
+            [3] = {1065.64,-377.23,67.92, 'Soprar folhas', "amb@code_human_wander_gardener_leaf_blower@idle_a", "idle_b", "prop_leaf_blower_01", 49, 28422},
+            [4] = {1049.79,-369.9,68.24, 'Consertar maçaneta', "amb@prop_human_parking_meter@female@idle_a", "idle_a_female" }
+        }
+    },
+
+    [5] = {
+        -- ['entrada'] = {1060.54,-378.09,68.24},
+        ['servicos'] = {
+            [1] = {470.95,-372.4,67.85, 'Soldar', "WORLD_HUMAN_WELDING" },
+            [2] = {1044.33,-372.88,67.9, 'Plantar semente', "amb@world_human_gardener_plant@female@base", "base_female"},
+            [3] = {1065.64,-377.23,67.92, 'Soprar folhas', "amb@code_human_wander_gardener_leaf_blower@idle_a", "idle_b", "prop_leaf_blower_01", 49, 28422},
+            [4] = {1049.79,-369.9,68.24, 'Consertar maçaneta', "amb@prop_human_parking_meter@female@idle_a", "idle_a_female" }
+        }
+    },
+}
 
 
-cfg.trabalho = 
-cfg.ganhoXp =
-cfg.xpUpar = 
-cfg.dinheiro = 
 
 
-
-
-cfg.locais = {
+--[[cfg.locais = {
 
     [1] = {
         ["nome"] =
@@ -69,6 +120,6 @@ cfg.locais = {
         ["coord-ped"] = 
     }
 
-}-]]
+}--]]
 
 return cfg
